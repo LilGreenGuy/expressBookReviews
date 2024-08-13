338a99c6c94b729ef5e616c69e83820f85b7edc6
Nick <nickbhenderson95@gmail.com>
2024-08-12 19:13:17 -0500

    Update index.js
    Making a stopping point. Will work on more tomorrow.

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,38 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
+
+// app.use("/friends", function auth(req, res, next) {
+//     // Check if user is logged in and has valid access token
+//     if (req.session.authorization) {
+//         let token = req.session.authorization['accessToken'];
+//         // Verify JWT token
+//         jwt.verify(token, "access", (err, user) => {
+//             if (!err) {
+//                 req.user = user;
+//                 next(); // Proceed to the next middleware
+//             } else {
+//                 return res.status(403).json({ message: "User not authenticated" });
+//             }
+//         });
+//     } else {
+//         return res.status(403).json({ message: "User not logged in" });
+//     }
+// });
  
 const PORT =5000;
 
