455a628381a4d77a944e79d38fff8d44e3bc5ff8
Nick H <nickbhenderson95@gmail.com>
2024-08-16 15:27:12 -0500

    Working on task 12 right now. Rewrote the author logic to work better

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,7 +10,7 @@ public_users.post("/register", (req, res) => {
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
-        if (isValid(username)) {
+        if (!isValid(username)) {
             // Add the new user to the users array
             users.push({ "username": username, "password": password });
             return res.status(200).json({ message: "User successfully registered. Now you can login" });
@@ -23,58 +23,88 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
+// public_users.get('/', async function (req, res) {
+//     res.send(JSON.stringify(books, null, 4))
+// });
+
+const bookPromise = new Promise((resolve, reject) => {
+    resolve(books)
+})
+
 public_users.get('/', async function (req, res) {
-    res.send(JSON.stringify(books, null, 4))
+    await bookPromise.then(
+    (book) => res.send(JSON.stringify(book, null, 4)),
+    (error) => res.send(error)
+    )
 });
 
+
 // Get book details based on ISBN
+// public_users.get('/isbn/:isbn', function (req, res) {
+//     const bookISBN = req.params.isbn
+//     if(!books[bookISBN]) {
+//         return res.status(400).json({message: "Please enter a valid number!"})
+//     }
+//     res.send(books[bookISBN]);
+// });
+
 public_users.get('/isbn/:isbn', function (req, res) {
-    const bookISBN = req.params.isbn
-    if(!bookISBN) {
+    const isbn = req.params.isbn
+    bookPromise.then(
+        (book) => res.send(book[isbn]),
+        (error) => res.send(error)
+    )
+    if(!books[isbn]) {
         return res.status(400).json({message: "Please enter a valid number!"})
     }
-    res.send(books[bookISBN]);
 });
 
 // Get book details based on author
+// public_users.get('/author/:author', function (req, res) {
+    // let answer = [];
+    // for(let book in books) {
+    //     if(books[book].author === req.params.author) {
+    //         answer.push(books[book]);
+    //     }
+    // }
+    // res.send(answer);
+// });
+
 public_users.get('/author/:author', function (req, res) {
-    //Write your code here
-    let ans = []
-    for(const [key, values] of Object.entries(books)){
-        const book = Object.entries(values);
-        for(let i = 0; i < book.length ; i++){
-            if(book[i][0] == 'author' && book[i][1] == req.params.author){
-                ans.push(books[key]);
-            }
+    let answer = [];
+    for(let book in books) {
+        if(books[book].author === req.params.author) {
+            answer.push(books[book]);
         }
+    }    
+    if(answer.length == 0){
+        return res.status(300).json({message: "Title not found"});
     }
-    if(ans.length == 0){
-        return res.status(300).json({message: "Author not found"});
-    }
-    res.send(ans);
+    res.send(answer);
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    let ans = []
-    for(const [key, values] of Object.entries(books)){
-        const book = Object.entries(values);
-        for(let i = 0; i < book.length ; i++){
-            if(book[i][0] == 'title' && book[i][1] == req.params.title){
-                ans.push(books[key]);
-            }
+    //Write your code here 
+    let answer = [];
+    for(let book in books) {
+        if(books[book].title === req.params.title) {
+            answer.push(books[book]);
         }
     }
-    if(ans.length == 0){
+    if(answer.length == 0){
         return res.status(300).json({message: "Title not found"});
     }
-    res.send(ans);
+    res.send(answer);
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const bookISBN = req.params.isbn
+    if(!bookISBN) {
+        return res.status(400).json({message: "Please enter a valid number!"})
+    }
+    res.send(books[bookISBN].reviews)
 });
 
 module.exports.general = public_users;
